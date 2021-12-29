d615377ef0f26280061b42fccc771056a00fca9e
Sean Stamm <sean.stamm5@gmail.com>
2021-12-29 15:22:05 -0800

    Add set fly n quack behavior methods to duck base

--- SimUDuck/Duck.java
+++ SimUDuck/Duck.java
@@ -18,4 +18,12 @@ public abstract class Duck {
     public void swim() {
         System.out.println("All ducks float, even decoys!");
     }
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
 }
